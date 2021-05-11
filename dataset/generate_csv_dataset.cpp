6f80aff349b6b690a23b308169bb567b963876ca
Ibaev Danil <danil.ibaev@yandex.ru>
2021-05-11 12:55:35 +0300

    add data

--- dataset/generate_csv_dataset.cpp
+++ dataset/generate_csv_dataset.cpp
@@ -21,37 +21,41 @@ int main(int argc, char** argv) {
   const auto path = string(kDatasetPath);  // конвертация string_view в string
   cout << "Dataset path: " << path << endl;
 
-  // Пример: чтение набора данных
-  auto input_stream = ifstream(path + "/dataset-example.csv");
-
-  if (input_stream) {
-    int line_number = 0;
-
-    // построчное чтение
-    for (string line; getline(input_stream, line); /* ... */) {
-      auto ss = stringstream(line);  // оборачиваем строку в объект "поток"
-
-      for (string token; getline(ss, token, ','); /* ... */) {
-        cout << "Token: [" << token << "] at line " << line_number << '\n';
-      }
-
-      cout << line << '\n';
-      line_number++;
-    }
-  }
+//  // Пример: чтение набора данных
+//  auto input_stream = ifstream(path + "/dataset-example.csv");
+//
+//  if (input_stream) {
+//    int line_number = 0;
+//
+//    // построчное чтение
+//    for (string line; getline(input_stream, line); /* ... */) {
+//      auto ss = stringstream(line);  // оборачиваем строку в объект "поток"
+//
+//      for (string token; getline(ss, token, ','); /* ... */) {
+//        cout << "Token: [" << token << "] at line " << line_number << '\n';
+//      }
+//
+//      cout << line << '\n';
+//      line_number++;
+//    }
+//  }
 
   // Пример: генерация набора данных
-  auto output_stream = ofstream(path + "/dataset-generated.csv", ios::ios_base::app);
-
+  auto output_stream = ofstream(path + "/insert/10/10000.csv", ios::ios_base::trunc);
   const auto seed = chrono::system_clock::now().time_since_epoch().count();
   auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
-  auto dist = uniform_int_distribution(0, 10);  // равновероятное распределение генерируемых чисел
-
+  auto  ves = uniform_int_distribution(1, 10);  // равновероятное распределение генерируемых чисел
+  auto rebra = uniform_int_distribution(10000, 10500);
+  int vershini = 10000;
+  auto versh = uniform_int_distribution(1, 10000);
+  int colvoreber = rebra(engine);
   if (output_stream) {
-    for (int counter = 0; counter < 10; counter++) {
-      output_stream << dist(engine) << ',';
+    output_stream << vershini << " ";
+    output_stream << colvoreber;
+    output_stream << endl;
+    for (int counter = 0; counter < colvoreber; counter++) {
+      output_stream << versh(engine) << " " << versh(engine) << " " << ves(engine) << endl;
     }
-    output_stream << dist(engine) << '\n';
   }
 
   return 0;
